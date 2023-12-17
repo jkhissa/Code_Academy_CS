a625e6f25700d80b4a2ef5c01ef36745e9f7804f
Jamal Issa <jkhissa@gmail.com>
2023-12-17 13:50:00 +0100

    files section experiments

---
+++ Files/script.py
@@ -0,0 +1,3 @@
+with open('/Users/jamalissa/Library/Mobile Documents/com~apple~CloudDocs/Python_Code_Academy/Files/keats_sonnet.txt') as keats_sonnet:
+        for line in keats_sonnet.readlines():
+            print(line)
---
+++ Files/with_statement.py
@@ -0,0 +1,11 @@
+with open('/Users/jamalissa/Library/Mobile Documents/com~apple~CloudDocs/Python_Code_Academy/Files/how_many_lines.txt') as how_many_lines:
+    for lines_doc in how_many_lines.readlines():
+        print(lines_doc)
+
+
+# Solution
+    # with open('how_many_lines.txt') as lines_doc:
+    #   for lines in lines_doc.readlines():
+    #     print(lines)
+        
+
